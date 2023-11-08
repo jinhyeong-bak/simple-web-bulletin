53d0f47e23c7f5c1a0cc7c230fbed5f48683eddf
jinhyeong-bak <realbrother98@naver.com>
2023-11-08 22:19:10 +0900

    init: 프로젝트 생성
    lombok, jdbc, thymeleaf, spring web 추가

---
+++ src/main/java/hello/simplewebbulletin/SimpleWebBulletinApplication.java
@@ -0,0 +1,13 @@
+package hello.simplewebbulletin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWebBulletinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleWebBulletinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/simplewebbulletin/SimpleWebBulletinApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.simplewebbulletin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebBulletinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
